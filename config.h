590af1cfbb777b720f29a8097edd6e7f016ddb6d
MiraiX <117575492+666hwll@users.noreply.github.com>
2024-10-27 18:50:48 +0100

    new config file

--- config.h
+++ config.h
@@ -7,10 +7,10 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=12" };
 static const char dmenufont[]       = "monospace:size=12";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
+static const char col_gray1[]       = "#67569D";
+static const char col_gray2[]       = "#87985B";
+static const char col_gray3[]       = "#1B00BB";
+static const char col_gray4[]       = "#1B0470";
 static const char col_cyan[]        = "#005577";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
